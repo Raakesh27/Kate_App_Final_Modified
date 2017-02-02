736c43a2d56a44540f1367712b69f19317a5b9d6
Wiehan <boshoff.2@wright.edu>
2017-02-01 22:07:56 -0500

    Added screan, button changes screan

---
+++ app/src/main/java/com/example/wiehan/kate_app_final/Activity_Two.java
@@ -0,0 +1,19 @@
+package com.example.wiehan.kate_app_final;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Activity_Two extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+}
+
