9ea406d48ec1a9c806636e38cd240b37e5ad4f8f
Wiehan <boshoff.2@wright.edu>
2017-02-03 15:57:19 -0500

    Added screan, button changes screan

--- app/src/main/java/com/example/wiehan/kate_app_final/Activity_Two.java
+++ app/src/main/java/com/example/wiehan/kate_app_final/Activity_Two.java
@@ -1,14 +1,27 @@
 package com.example.wiehan.kate_app_final;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_Two extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_two);
+
+        Button viewVitalsButton = (Button) findViewById(R.id.viewVitalsButton);
+
+        viewVitalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Activity_Vitals.class);
+                startActivity(intent);
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/example/wiehan/kate_app_final/Activity_Vitals.java
@@ -0,0 +1,24 @@
+package com.example.wiehan.kate_app_final;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity_Vitals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vitals);
+
+
+
+
+
+
+    }
+
+}
+
