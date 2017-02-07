37312029693cc7ea949ce1d57355ebe0653a7df7
Wiehan <boshoff.2@wright.edu>
2017-02-06 20:40:49 -0500

    Scenario A and B Complete, Scenario C up until Activity_Vitals_ScenThree

---
+++ app/src/main/java/com/example/wiehan/kate_app_final/Activity_Vitals_ScenThree.java
@@ -0,0 +1,13 @@
+package com.example.wiehan.kate_app_final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Vitals_ScenThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vitals_scen_three);
+    }
+}
